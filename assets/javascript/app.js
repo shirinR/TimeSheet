fe1f3069d259d524f0380bfe09f4c8d9baf50121
Momojunzi <mark.holthaus@gmail.com>
2017-08-26 11:59:55 -0700

    fourth push

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,4 +26,24 @@ var config = {
 
   });
 
+  database.ref().on('child_added', function(snapshot){
+  	var data = snapshot.val();
+  	var name=data.name;
+  	var role=data.role;
+  	var start=data.start;
+  	var rate=data.rate;
+
+  	$("#tbody").append("<tr><td id='name'> " + name +
+        " </td><td id='email'> " + role +
+        " </td><td id='age'> " + start +
+        " </td><td id='comment'> " + rate + " </td></tr>");
+
+  	console.log(data.name);
+  	console.log(data.role);
+  	console.log(data.start);
+  	console.log(data.rate);
+  }, function(errorObject){
+  	console.log(errorObject);
+  });
+
   
